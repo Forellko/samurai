a56bc78d498916c433347f1f1082ffc6ae3543f8
forellko <dragonforce97q@gmail.com>
2021-09-11 00:22:05 +0300

    add all files

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <img
+        src="https://pbs.twimg.com/profile_images/1063925348205821958/DlGcxdOl_400x400.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div>
+        <img
+          src="https://st2.depositphotos.com/1021974/11917/i/950/depositphotos_119175322-stock-photo-colorful-cubes-illustration.jpg"
+          alt="image_in_profile"
+        ></img>
+      </div>
+      <div>ava + desc</div>
+      <div>
+        My posts
+        <div>New post</div>
+      </div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+        <div>post 3</div>
+      </div>
+      profile
+    </div>
+  );
+};
+
+export default Profile;
